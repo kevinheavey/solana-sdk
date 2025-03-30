4d1e935a58ff123eba6cefeb6a9f7b05b269543f
Kevin Heavey <heaveykevin2@gmail.com>
2025-03-30 12:27:51 +0100

    make std optional in solana-msg

--- msg/src/lib.rs
+++ msg/src/lib.rs
@@ -1,3 +1,6 @@
+#![no_std]
+#[cfg(feature = "std")]
+extern crate std;
 /// Print a message to the log.
 ///
 /// Supports simple strings as well as Rust [format strings][fs]. When passed a
@@ -25,6 +28,7 @@
 /// let err = "not enough signers";
 /// msg!("multisig failed: {}", err);
 /// ```
+#[cfg(feature = "std")]
 #[macro_export]
 macro_rules! msg {
     ($msg:expr) => {
@@ -38,12 +42,12 @@ pub mod syscalls;
 
 /// Print a string to the log.
 #[inline]
-pub fn sol_log(message: &str) {
+pub fn sol_log(_message: &str) {
     #[cfg(target_os = "solana")]
     unsafe {
-        syscalls::sol_log_(message.as_ptr(), message.len() as u64);
+        syscalls::sol_log_(_message.as_ptr(), _message.len() as u64);
     }
 
-    #[cfg(not(target_os = "solana"))]
-    println!("{message}");
+    #[cfg(all(not(target_os = "solana"), feature = "std"))]
+    std::println!("{_message}");
 }
